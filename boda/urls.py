a4a592f90f52dfa2194ffecd0ba50cb4a5eb2e53
virsail <virsaileric@gmail.com>
2020-11-03 16:20:30 +0300

    deine reigister api url in urls.py

--- boda/urls.py
+++ boda/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('api/register/', RegisterAPI.as_view(), name='register'),
+    url('api/register/', RegisterAPI.as_view(), name='register'),
 ]
