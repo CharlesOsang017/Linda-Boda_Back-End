963407d89cfadd70c4c0b76d1773b56f7f5ffba2
virsail <virsaileric@gmail.com>
2020-11-03 15:55:11 +0300

    make knox authentication my defualt authentication class in djangorf

--- linda/settings.py
+++ linda/settings.py
@@ -41,6 +41,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'knox'
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'knox.auth.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
