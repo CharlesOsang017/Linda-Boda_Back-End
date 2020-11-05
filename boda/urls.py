d86f1f9bbaff80c45c4139bee66d2ac653e0a6b6
virsail <virsaileric@gmail.com>
2020-11-05 13:32:22 +0300

    add the login urls

--- boda/urls.py
+++ boda/urls.py
@@ -1,10 +1,9 @@
-from .views import RegisterAPI
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static 
-
+from knox import views as knox_views
+from .views import LoginAPI
+from django.urls import path
 
 urlpatterns = [
-    url(r'^api/register/$', RegisterAPI.as_view(), name='register'),
+    url(r'^api/login/$', LoginAPI.as_view(), name='login'),
+    url(r'^api/logout/$', knox_views.LogoutView.as_view(), name='logout'),
+    url(r'^api/logoutall/$', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
